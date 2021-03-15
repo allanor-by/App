b767dcb6a864539b7bd2b4b1e0787eb2b913445f
allanor-by <63023090+allanor-by@users.noreply.github.com>
2021-03-15 15:12:40 +0300

    Update fsa_page.php

--- For_SmartApp/includes/fsa_page.php
+++ For_SmartApp/includes/fsa_page.php
@@ -2,3 +2,28 @@
  <h1>For smartApp</h1>
  <p>link to the form <a href="/wp-content/plugins/For_SmartApp/public/post.php">here</p>
 </div>
+<p> or paste this code </p>
+<code><html>
+<head>
+<title>Custom Form</title>
+</head>
+<body>
+  <h2>Custom Form</h2>
+  <form action="./mail.php" method="post">
+  <fieldset>
+  <legend>Custom Form:</legend>
+  First Name:
+  <input type="text" name="fname"><br><br>
+ Last Name:
+  <input type="text" name="lname"><br><br>
+  Subject:
+  <input type="text" name="Subject"><br><br>
+   Message:
+  <textarea rows="10" cols="45" name="message"></textarea><br><br>
+  E-mail:
+  <input type="text" name="email"><br><br>
+  <input type="submit" value="Send mail">
+  </fieldset>
+  </form>
+</body>
+</html></code>
